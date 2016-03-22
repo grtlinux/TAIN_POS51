32a98cbbd6726c80a4f4201dc6a69e06ca7a83c5
Kiea Seok Kang <grtlinux@gmail.com>
2016-03-22 20:35:14 +0900

    .

---
+++ TAIN_POS51/src/tain/kr/com/proj/pos51/v02/main/Pos51Main.java
@@ -0,0 +1,48 @@
+/**
+ * Copyright 2014, 2015, 2016 TAIN, Inc. all rights reserved.
+ *
+ * Licensed under the GNU GENERAL PUBLIC LICENSE, Version 3, 29 June 2007 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.gnu.org/licenses/
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * -----------------------------------------------------------------
+ * Copyright 2014, 2015, 2016 TAIN, Inc.
+ *
+ */
+package tain.kr.com.proj.pos51.v02.main;
+
+import org.apache.log4j.Logger;
+
+/**
+ * Code Templates > Comments > Types
+ *
+ * <PRE>
+ *   -. FileName   : Pos51Main.java
+ *   -. Package    : tain.kr.com.proj.pos51.v02.main
+ *   -. Comment    :
+ *   -. Author     : taincokr
+ *   -. First Date : 2016. 3. 22. {time}
+ * </PRE>
+ *
+ * @author taincokr
+ *
+ */
+public class Pos51Main {
+
+	private static boolean flag = true;
+
+	private static final Logger log = Logger.getLogger(Pos51Main.class);
+
+	///////////////////////////////////////////////////////////////////////////////////////////////
+	///////////////////////////////////////////////////////////////////////////////////////////////
+	///////////////////////////////////////////////////////////////////////////////////////////////
+
+}
