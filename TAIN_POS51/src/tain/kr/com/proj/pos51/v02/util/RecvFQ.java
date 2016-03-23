47fab754ca9518628d5bb48876be9e31f21d581b
Kiea Seok Kang <grtlinux@gmail.com>
2016-03-23 14:40:55 +0900

    .

--- TAIN_POS51/src/tain/kr/com/proj/pos51/v02/util/RecvFQ.java
+++ TAIN_POS51/src/tain/kr/com/proj/pos51/v02/util/RecvFQ.java
@@ -19,6 +19,11 @@
  */
 package tain.kr.com.proj.pos51.v02.util;
 
+import java.io.RandomAccessFile;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
 import org.apache.log4j.Logger;
 
 /**
@@ -42,7 +47,139 @@ public class RecvFQ {
 	private static final Logger log = Logger.getLogger(RecvFQ.class);
 
 	///////////////////////////////////////////////////////////////////////////////////////////////
+	
+	private static final String REC_FILE_NAME = "POSHW000002";
+	
+	private String fileName = null;
+	private String recvName = null;
+	private int recCnt = -1;
+	
+	private RandomAccessFile raf = null;
+	private String line = null;
+	private byte[] byLine = null;
+	private int recSeq = -1;
+	private int recvSeq = -1;
+	private long pos = -1;
+	private int recLen = -1;
+
+	private String strDate = null;
+	private String strTime = null;
+	
+	public RecvFQ(String fileName, String recvName, int recCnt) throws Exception {
+		
+		if (flag) {
+			this.fileName = fileName;
+			this.recvName = recvName;
+			this.recCnt = recCnt;
+			
+			this.raf = new RandomAccessFile(this.fileName, "rw");
+		}
+		
+		if (flag) {
+			int idx = this.recvName.lastIndexOf("recv_");
+			if (idx < 1)
+				throw new Exception("ERROR : wrong file name : [" + this.recvName + "]");
+			
+			this.strDate = this.recvName.substring(idx +  5, idx + 13);
+			this.strTime = this.recvName.substring(idx + 13, idx + 19);
+		}
+	}
+	
+	private void readAll() throws Exception {
+		
+		if (flag) {
+			
+			this.recSeq = 0;
+			this.recvSeq = 0;
+			while (true) {
+				this.pos = this.raf.getFilePointer();
+				this.line = this.raf.readLine();
+				if (this.line == null)
+					break;
+				
+				this.recSeq ++;
+
+				this.recLen = this.line.length();
+				if (flag) log.debug(String.format("(%02d) (%05d) [%4d:%s]", this.recSeq, this.pos, this.recLen, this.line));
+				
+				this.byLine = this.line.getBytes();
+				
+				String strRecFile = FqType.REC_FILE.getString(this.byLine);
+				if (flag) log.debug("REC_FILE : [" + strRecFile + "]");
+				
+				if (REC_FILE_NAME.equals(strRecFile)) {
+					this.recvSeq ++;
+				}
+			}
+		}
+	}
+
+	public void write() throws Exception {
+		
+		if (flag) {
+			this.raf.seek(this.pos);
+			
+			this.byLine = FqType.makeBytes();
+
+			if (flag) {
+				FqType.FQ_SEQ  .setVal(this.byLine, String.valueOf(this.recSeq));
+				FqType.FQ_RDR  .setVal(this.byLine, "");
+				FqType.FQ_LEN  .setVal(this.byLine, "101");
+				FqType.REC_LEN .setVal(this.byLine, "671");
+				FqType.REC_CNT .setVal(this.byLine, String.valueOf(this.recCnt));
+				FqType.REC_SR  .setVal(this.byLine, "R");
+				FqType.REC_ORG .setVal(this.byLine, "POS");
+				FqType.REC_FILE.setVal(this.byLine, REC_FILE_NAME);
+				FqType.REC_FSIZ.setVal(this.byLine, "11");
+				FqType.REC_DATE.setVal(this.byLine, this.strDate);
+				FqType.REC_TIME.setVal(this.byLine, this.strTime);
+				FqType.REC_SEQ .setVal(this.byLine, String.valueOf(++ this.recvSeq));
+				FqType.FILLER  .setVal(this.byLine, "");
+			}
+			
+			this.raf.write(this.byLine);
+		}
+	}
+	
+	public void close() throws Exception {
+		
+		if (flag) {
+			if (raf != null) {
+				try {
+					raf.close();
+				} catch (Exception e) {
+					// e.printStackTrace();
+				}
+			}
+		}
+	}
+	
 	///////////////////////////////////////////////////////////////////////////////////////////////
 	///////////////////////////////////////////////////////////////////////////////////////////////
 
+	private static void test01(String[] args) throws Exception {
+		
+		if (!flag) {
+			/*
+			 * just read all records from the file OASPOS5101
+			 */
+			
+			String fileName = "N:/TEMP/TEST/HANWA/recv/DAT/20160323/OARPOS5101";
+			String recvName = "N:/TEMP/TEST/POST_AGENT/receive/recv_20160323141728.txt";
+			int recCnt = 123;
+			
+			RecvFQ recvFQ = new RecvFQ(fileName, recvName, recCnt);
+			
+			recvFQ.readAll();
+			
+			recvFQ.close();
+		}
+	}
+	
+	public static void main(String[] args) throws Exception {
+		
+		if (flag) log.debug(">>>>> " + new Object(){}.getClass().getEnclosingClass().getName());
+		
+		if (flag) test01(args);
+	}
 }
