cc65c3a7fdb0505ff36f27e3ac2417395fbe34da
Kiea Seok Kang <grtlinux@gmail.com>
2016-03-23 16:08:05 +0900

    .

--- TAIN_POS51/src/tain/kr/com/proj/pos51/v02/tools/ReceiveResult02.java
+++ TAIN_POS51/src/tain/kr/com/proj/pos51/v02/tools/ReceiveResult02.java
@@ -19,9 +19,12 @@
  */
 package tain.kr.com.proj.pos51.v02.tools;
 
+import java.io.File;
 import java.util.ResourceBundle;
 
 import org.apache.log4j.Logger;
+
+import tain.kr.com.proj.pos51.v02.util.DateTime;
 
 /**
  * Code Templates > Comments > Types
@@ -49,6 +52,20 @@ public class ReceiveResult02 extends Thread {
 	
 	private String clsName = null;
 	
+	private String strDescription = null;
+
+	private String strInFileCharset = null;
+	private String strInFilePath = null;
+	private String strInFileName = null;
+
+	private String strMvFilePath = null;
+	
+	private String strOutFileCharset = null;
+	private String strOutFilePath = null;
+	private String strOutFileName = null;
+
+	private String strFqFilePatn = null;
+	private String strFqFileName = null;
 	
 	public ReceiveResult02() throws Exception {
 		
@@ -59,11 +76,76 @@ public class ReceiveResult02 extends Thread {
 			
 			ResourceBundle rb = ResourceBundle.getBundle(this.clsName.replace('.','/'));
 			
+			this.strDescription    = rb.getString("tain.pos51.02.receive.result.desc" );
+
+			this.strInFileCharset  = rb.getString("tain.pos51.02.recv.in.file.charset" );
+			this.strInFilePath     = rb.getString("tain.pos51.02.recv.in.file.path"    );
+			this.strInFileName     = rb.getString("tain.pos51.02.recv.in.file.name"    );
+
+			this.strMvFilePath     = rb.getString("tain.pos51.02.recv.mv.file.path"    );
+
+			this.strOutFileCharset = rb.getString("tain.pos51.02.recv.out.file.charset");
+			this.strOutFilePath    = rb.getString("tain.pos51.02.recv.out.file.path"   );
+			this.strOutFileName    = rb.getString("tain.pos51.02.recv.out.file.name"   );
+
+			this.strFqFilePatn     = rb.getString("tain.pos51.02.recv.fq.file.path"    );
+			this.strFqFileName     = rb.getString("tain.pos51.02.recv.fq.file.name"    );
+		}
+
+		if (flag) {
+			/*
+			 * System.properties
+			 */
+			this.strDescription    = System.getProperty("tain.pos51.02.receive.request.desc" , this.strDescription   );
+
+			this.strInFileCharset  = System.getProperty("tain.pos51.02.recv.in.file.charset" , this.strInFileCharset );
+			this.strInFilePath     = System.getProperty("tain.pos51.02.recv.in.file.path"    , this.strInFilePath    );
+			this.strInFileName     = System.getProperty("tain.pos51.02.recv.in.file.name"    , this.strInFileName    );
+
+			this.strMvFilePath     = System.getProperty("tain.pos51.02.recv.mv.file.path"    , this.strMvFilePath    );
+
+			this.strOutFileCharset = System.getProperty("tain.pos51.02.recv.out.file.charset", this.strOutFileCharset);
+			this.strOutFilePath    = System.getProperty("tain.pos51.02.recv.out.file.path"   , this.strOutFilePath   );
+			this.strOutFileName    = System.getProperty("tain.pos51.02.recv.out.file.name"   , this.strOutFileName   );
+
+			this.strFqFilePatn     = System.getProperty("tain.pos51.02.recv.fq.file.path"    , this.strFqFilePatn    );
+			this.strFqFileName     = System.getProperty("tain.pos51.02.recv.fq.file.name"    , this.strFqFileName    );
+		}
+
+		if (flag) {
+			/*
+			 * change parameters
+			 */
+			this.strInFileName  = this.strInFilePath + File.separator + this.strInFileName;
+			this.strInFileName  = this.strInFileName.replaceAll("YYYYMMDDHHMMSS", DateTime.getInstance().getYYYYMMDDHHMMSS());
+
+			this.strOutFileName = this.strOutFilePath + File.separator + this.strOutFileName;
+			//this.strOutFileName = this.strOutFileName.replaceAll("YYYYMMDD", DateTime.getInstance().getYYYYMMDD());
+
+			this.strFqFileName  = this.strFqFilePatn + File.separator + this.strFqFileName;
+			this.strFqFileName  = this.strFqFileName.replaceAll("YYYYMMDD", DateTime.getInstance().getYYYYMMDD());
+		}
+
+		if (flag) {
+			log.debug("FQ  FILE : " + this.strFqFileName);
+			log.debug("IN  FILE : " + this.strInFileName);
+			log.debug("OUT FILE : " + this.strOutFileName);
 		}
 	}
 	
 	public void run() {
 		
+		if (flag) {
+			/*
+			 * if the file FQ is not exist, then this module's going to end...
+			 */
+			File file = new File(this.strFqFileName);
+			if (!file.exists()) {
+				if (flag) log.error("ERROR : the file doesn't exist..[" + this.strFqFileName + "]");
+				return;
+			}
+		}
+
 		if (flag) {
 			
 			try {
